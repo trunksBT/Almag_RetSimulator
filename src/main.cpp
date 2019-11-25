91a94f11759d016ea9fcd60cb4b2be120d02c060
pkorycin <pkorycin>
2019-11-25 21:56:21 +0100

    Add logger

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
+#include <Utils/Logger.hpp>
+
 int main()
 {
-   std::cout << "Hello world" << std::endl;
+   init_logger(!IS_LOG_TO_FILE, IS_LOG_ON_STD_OUT,
+               boost::log::trivial::trace);
+
+   LOG(trace) << "BEGIN";
+   LOG(trace) << "END";
    return 0;
 }
