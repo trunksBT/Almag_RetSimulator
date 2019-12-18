f47f48f725793ed12e1bf884b83169ea03c84703
pkorycin <pkorycin>
2019-12-18 17:55:30 +0100

    Prepare for receiving message from the RetDriverSimulator

--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,14 @@
 #include <Controller/CmdValidationRules/DatabaseCommandValidationManager.hpp>
 
 #include <Utils/UserCommandParser.hpp>
+#include <TestUtils/HDLC/DataLinkLayerCommunicators/ZMqReqRespCommunicator.hpp>
 
 using namespace std;
 
-int main()
+void runTheConsoleMode()
 {
-   init_logger(IS_LOG_TO_FILE, IS_LOG_ON_STD_OUT,
-               boost::log::trivial::trace);
+   LOG(debug);
 
-   LOG(trace) << "BEGIN";
    Database db({});
    std::vector<IHDLCCommunicatorPtr> hdlcCommunicators {{
       std::make_shared<HDLCCommunicator>(),
@@ -29,8 +28,32 @@ int main()
    AlmagControllerPtr ctrl = std::make_shared<AlmagController>(db, commandFactory);
 
    CMenu ui{"AlmagRetUI", "_", db, ctrl,
-            std::make_shared<AlmagCommandValidationManager>(db),
-            std::make_unique<DatabaseCommandValidationManager>(db)
+       std::make_shared<AlmagCommandValidationManager>(db),
+       std::make_unique<DatabaseCommandValidationManager>(db)
+   };
+
+   ui.setAlmagCommandsConstraints({
+       constraints::almag::values.begin(), constraints::almag::values.end()});
+   ui.setDatabaseCommandsConstraints({
+       constraints::database::values.begin(), constraints::database::values.end()});
+
+   ui.run({});
+}
+
+void runTheInterpretMode()
+{
+   LOG(debug);
+
+   Database db({});
+   std::vector<IHDLCCommunicatorPtr> hdlcCommunicators {{
+      std::make_shared<ZMqReqRespCommunicator>(),
+   }};
+   ICommandFactoryPtr commandFactory = std::make_shared<RetCommandFactory>(hdlcCommunicators);
+   AlmagControllerPtr ctrl = std::make_shared<AlmagController>(db, commandFactory);
+
+   CMenu ui{"AlmagRetUI", "_", db, ctrl,
+      std::make_shared<AlmagCommandValidationManager>(db),
+      std::make_unique<DatabaseCommandValidationManager>(db)
    };
 
    ui.setAlmagCommandsConstraints({
@@ -38,8 +61,18 @@ int main()
    ui.setDatabaseCommandsConstraints({
       constraints::database::values.begin(), constraints::database::values.end()});
 
+   hdlcCommunicators.at(0)->setupReceive("Dupa");
+   hdlcCommunicators.at(0)->receive("Dupa");
    ui.run({});
+}
 
+int main()
+{
+   init_logger(IS_LOG_TO_FILE, IS_LOG_ON_STD_OUT,
+               boost::log::trivial::trace);
+
+   LOG(trace) << "BEGIN";
+   runTheInterpretMode();
    LOG(trace) << "END";
    return 0;
 }
