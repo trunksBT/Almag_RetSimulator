a038a7de07dbd74d49609e7a5b81032a88adebc3
trunksBT <korytko07@gmail.com>
2020-01-06 13:34:04 +0100

    Receive the message from ret driver simulator (#5)
    * Prepare for receiving message from the RetDriverSimulator
    
    * Add the MT-s for AlmagController
    
    * Fix the mt compilation
    
    * Fix linking the mt
    
    * Add tests
    
    * Finish implementing the frame to command translator
    
    * Add tests for the translating all messages from frame to cmd

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,55 @@
 #include <Controller/AlmagController.hpp>
 #include <Database/Database.hpp>
+#include <Database/DatabaseConstraints.hpp>
 #include <HDLC/HDLCCommunicator.hpp>
+
 #include <UserInterface/CMenu.hpp>
 
-#include <Utils/Logger.hpp>
-
 #include <PluginSpecifics/CmdConstraints/AlmagConstraints.hpp>
-#include <Database/DatabaseConstraints.hpp>
 #include <PluginSpecifics/RetCommandFactory.hpp>
+#include <PluginSpecifics/HDLCFrameToResponseCommandTranslator.hpp>
+
 #include <Controller/CmdValidationRules/AlmagCommandValidationManager.hpp>
 #include <Controller/CmdValidationRules/DatabaseCommandValidationManager.hpp>
 
+#include <Utils/Logger.hpp>
 #include <Utils/UserCommandParser.hpp>
+#include <TestUtils/HDLC/DataLinkLayerCommunicators/ZMqReqRespCommunicator.hpp>
 
 using namespace std;
 
 int main()
 {
    init_logger(IS_LOG_TO_FILE, IS_LOG_ON_STD_OUT,
-               boost::log::trivial::trace);
+       boost::log::trivial::trace);
 
-   LOG(trace) << "BEGIN";
+   LOG(debug);
+
    Database db({});
    std::vector<IHDLCCommunicatorPtr> hdlcCommunicators {{
-      std::make_shared<HDLCCommunicator>(),
+       std::make_shared<ZMqReqRespCommunicator>(zmq::socket_type::rep),  // release mode
+//      std::make_shared<HDLCCommunicator>(),  // debug mode
    }};
    ICommandFactoryPtr commandFactory = std::make_shared<RetCommandFactory>(hdlcCommunicators);
    AlmagControllerPtr ctrl = std::make_shared<AlmagController>(db, commandFactory);
 
    CMenu ui{"AlmagRetUI", "_", db, ctrl,
-            std::make_shared<AlmagCommandValidationManager>(db),
-            std::make_unique<DatabaseCommandValidationManager>(db)
+       std::make_shared<AlmagCommandValidationManager>(db),
+       std::make_unique<DatabaseCommandValidationManager>(db)
    };
 
    ui.setAlmagCommandsConstraints({
-      constraints::almag::values.begin(), constraints::almag::values.end()});
+       constraints::almag::values.begin(), constraints::almag::values.end()});
    ui.setDatabaseCommandsConstraints({
-      constraints::database::values.begin(), constraints::database::values.end()});
+       constraints::database::values.begin(), constraints::database::values.end()});
 
-   ui.run({});
+   HDLCFrameToResponseCommandTranslator frameTranslator{};
+
+   const std::string hardcodedPort = "5555";
+   auto receivedHdlcFrame = hdlcCommunicators.at(0)->receive(hardcodedPort);  // release mode
+   auto commandToExecute = frameTranslator.translate(receivedHdlcFrame->getFrameBody());
+   //  here for command interpreter
+   ui.runPredefinedCommands({{commandToExecute, hardcodedPort}});
 
    LOG(trace) << "END";
    return 0;
---
+++ test/ut/HDLCFrameToResponseCommandTranslator.cpp
@@ -0,0 +1,68 @@
+#include <gmock/gmock.h>
+
+#include <PluginSpecifics/CmdConstraints/AlmagConstraints.hpp>
+#include <PluginSpecifics/HDLCFrameToResponseCommandTranslator.hpp>
+#include <HDLC/HDLCFrameBodyInterpreter.hpp>
+#include <TestUtils/HDLC/FramesFactories/SRetHDLCReqFrameBodyStrFactory.hpp>
+#include <TestUtils/HDLC/FramesFactories/FrameStrFactory.hpp>
+#include <TestUtils/StructsForParametrizedTests.hpp>
+
+using testing::Eq;
+using namespace constraints::almag;
+
+namespace
+{
+FrameStrFactoryPtr retDeviceStrFactory = std::make_shared<SRetHDLCReqFrameBodyStrFactory>();
+}
+
+class HDLCFrameToResponseCommandTranslatorTests : public ::testing::TestWithParam<ReceivedFrameStr_RespCommandStr>
+{
+public:
+   HDLCFrameToResponseCommandTranslatorTests()
+   : cmdTranslator{}
+   , frameInterpreter{}
+   {}
+
+   HDLCFrameToResponseCommandTranslator cmdTranslator;
+   HDLCFrameBodyInterpreter frameInterpreter;
+};
+
+TEST_P(HDLCFrameToResponseCommandTranslatorTests, BuiltFrame)
+{
+   auto recFrame{HDLCFrameBodyInterpreter().apply(GetParam().receivedFrameStr) };
+   ASSERT_THAT(cmdTranslator.translate(recFrame),Eq(GetParam().respCommandStr));
+}
+
+INSTANTIATE_TEST_CASE_P(HDLCFrameToResponseCommandTranslatorTests,
+        HDLCFrameToResponseCommandTranslatorTests,
+        ::testing::Values(
+        ReceivedFrameStr_RespCommandStr{
+            retDeviceStrFactory->get_FrameXID_DeviceScan(),
+            L2::DEVICE_SCAN
+        },
+        ReceivedFrameStr_RespCommandStr{
+            retDeviceStrFactory->get_FrameXID_AddressAssignment(),
+            L2::ADDRESS_ASSIGNMENT
+        },
+        ReceivedFrameStr_RespCommandStr{
+            retDeviceStrFactory->get_FrameU_LinkEstablishment(),
+            L2::LINK_ESTABLISHMENT
+        },
+        ReceivedFrameStr_RespCommandStr{
+            retDeviceStrFactory->get_FrameXID_HDLCParameters(),
+            L2::HDLC_PARAMETERS
+        },
+        ReceivedFrameStr_RespCommandStr{
+            retDeviceStrFactory->get_FrameXID_3GPPReleaseId(),
+            L2::THREEGPP_RELEASE_ID
+        },
+        ReceivedFrameStr_RespCommandStr{
+            retDeviceStrFactory->get_FrameXID_AISGProtocolVersion(),
+            L2::AISG_PROTOCOL_VERSION
+        },
+        ReceivedFrameStr_RespCommandStr{
+            retDeviceStrFactory->get_FrameI_Calibrate(),
+            L7::CALIBRATE
+        }
+    )
+);
