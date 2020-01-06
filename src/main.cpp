7da9b9c9066348beef8cb2b3a133878e1f1a3c14
pkorycin <pkorycin>
2020-01-06 11:37:07 +0100

    Add the MT-s for AlmagController

--- src/main.cpp
+++ src/main.cpp
@@ -16,13 +16,17 @@
 
 using namespace std;
 
-void runTheConsoleMode()
+int main()
 {
+   init_logger(IS_LOG_TO_FILE, IS_LOG_ON_STD_OUT,
+       boost::log::trivial::trace);
+
    LOG(debug);
 
    Database db({});
    std::vector<IHDLCCommunicatorPtr> hdlcCommunicators {{
-      std::make_shared<HDLCCommunicator>(),
+       std::make_shared<ZMqReqRespCommunicator>(zmq::socket_type::rep),  // release mode
+//      std::make_shared<HDLCCommunicator>(),  // debug mode
    }};
    ICommandFactoryPtr commandFactory = std::make_shared<RetCommandFactory>(hdlcCommunicators);
    AlmagControllerPtr ctrl = std::make_shared<AlmagController>(db, commandFactory);
@@ -37,42 +41,10 @@ void runTheConsoleMode()
    ui.setDatabaseCommandsConstraints({
        constraints::database::values.begin(), constraints::database::values.end()});
 
+   hdlcCommunicators.at(0)->receive("5555");  // release mode
+   //  here for command interpreter
    ui.run({});
-}
 
-void runTheInterpretMode()
-{
-   LOG(debug);
-
-   Database db({});
-   std::vector<IHDLCCommunicatorPtr> hdlcCommunicators {{
-      std::make_shared<ZMqReqRespCommunicator>(),
-   }};
-   ICommandFactoryPtr commandFactory = std::make_shared<RetCommandFactory>(hdlcCommunicators);
-   AlmagControllerPtr ctrl = std::make_shared<AlmagController>(db, commandFactory);
-
-   CMenu ui{"AlmagRetUI", "_", db, ctrl,
-      std::make_shared<AlmagCommandValidationManager>(db),
-      std::make_unique<DatabaseCommandValidationManager>(db)
-   };
-
-   ui.setAlmagCommandsConstraints({
-      constraints::almag::values.begin(), constraints::almag::values.end()});
-   ui.setDatabaseCommandsConstraints({
-      constraints::database::values.begin(), constraints::database::values.end()});
-
-   hdlcCommunicators.at(0)->setupReceive("Dupa");
-   hdlcCommunicators.at(0)->receive("Dupa");
-   ui.run({});
-}
-
-int main()
-{
-   init_logger(IS_LOG_TO_FILE, IS_LOG_ON_STD_OUT,
-               boost::log::trivial::trace);
-
-   LOG(trace) << "BEGIN";
-   runTheInterpretMode();
    LOG(trace) << "END";
    return 0;
 }
---
+++ test/mt/AlmagControllerTests.cpp
@@ -0,0 +1,85 @@
+#include <TestUtils/Fixtures/BaseFixtureWithDBAndHDLC.hpp>
+
+#include <Controller/AlmagController.hpp>
+#include <Controller/IController.hpp>
+
+#include <PluginSpecifics/CmdConstraints/AlmagConstraints.hpp>
+#include <PluginSpecifics/RetCommandFactory.hpp>
+
+#include <TestUtils/Hardcodes.hpp>
+#include <TestUtils/StructsForParametrizedTests.hpp>
+#include <TestUtils/HDLC/DataLinkLayerCommunicators/RoundTripHDLCCommunicatorStub.hpp>
+#include <Utils/Utils.hpp>
+
+using namespace command;
+using namespace defaultVals;
+using namespace constraints::almag;
+using namespace hardcodes::IOPaths;
+
+namespace mt
+{
+class AlmagControllerShouldPar:
+        public BaseFixtureWithDBAndHDLC,
+        public ::testing::WithParamInterface<CommandsToExpectedFrame>
+{
+protected:
+   AlmagControllerShouldPar()
+      : BaseFixtureWithDBAndHDLC({}, {std::make_shared<test::RoundTripHDLCCommunicatorStub>()})
+      , ctrl_(std::make_shared<AlmagController>(db_, std::make_shared<RetCommandFactory>(hdlcCommunicators_)))
+   {};
+
+   IAlmagControllerPtr ctrl_;
+};
+
+TEST_P(AlmagControllerShouldPar, Execute_Command_Expect_ResultCode)
+{
+   ctrl_->addCommands(GetParam().inCommands);
+   ctrl_->executeCommand();
+
+   ASSERT_THAT(ctrl_->getFinalResultCode(), StrEq(GetParam().expectedHdlcFrame));
+}
+
+INSTANTIATE_TEST_CASE_P(BaseFixtureWithDBAndHDLC,
+        AlmagControllerShouldPar,
+        ::testing::Values(
+//        CommandsToExpectedFrame{
+//                {{ L1::DUMMY_SCAN, BUFFER_TO_SEND_VAL_1 }},
+//                L1::DUMMY_SCAN + DELIMITER
+//        },
+//        CommandsToExpectedFrame{
+//                {{ L1::SET_LINK_SPEED, BUFFER_TO_SEND_VAL_1 }},
+//                multiplyString(NUMBER_OF_DUMMY_SCANS_FOR_9_6_KBPS, L1::DUMMY_SCAN + DELIMITER)
+//        },
+        CommandsToExpectedFrame{
+                {{ L2::DEVICE_SCAN, BUFFER_TO_SEND_VAL_1 }},
+                L2::DEVICE_SCAN + DELIMITER
+        },
+        CommandsToExpectedFrame{
+                {{ L2::ADDRESS_ASSIGNMENT, BUFFER_TO_SEND_VAL_1 }},
+                L2::ADDRESS_ASSIGNMENT + DELIMITER
+        }
+//        CommandsToExpectedFrame{
+//                {{ L2::LINK_ESTABLISHMENT, BUFFER_TO_SEND_VAL_1 }},
+//                L2::LINK_ESTABLISHMENT + DELIMITER
+//        },
+//        CommandsToExpectedFrame{
+//                {{ L2::HDLC_PARAMETERS, BUFFER_TO_SEND_VAL_1 }},
+//                L2::HDLC_PARAMETERS + DELIMITER
+//        },
+//        CommandsToExpectedFrame{
+//                {{ L2::THREEGPP_RELEASE_ID, BUFFER_TO_SEND_VAL_1 }},
+//                L2::THREEGPP_RELEASE_ID + DELIMITER
+//        },
+//        CommandsToExpectedFrame{
+//                {{ L2::AISG_PROTOCOL_VERSION, BUFFER_TO_SEND_VAL_1 }},
+//                L2::AISG_PROTOCOL_VERSION + DELIMITER
+//        },
+//        CommandsToExpectedFrame{
+//                {{ L7::CALIBRATE, BUFFER_TO_SEND_VAL_1 }},
+//                L7::CALIBRATE + DELIMITER
+//        }
+   )
+)
+
+}  // namespace mt
+
